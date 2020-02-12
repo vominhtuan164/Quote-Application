b055e8fd7a1dbe5280382f0f5ff41b5e39071ca8
Minh Tuan VO <vominhtuan164@gmail.com>
2020-02-12 23:01:13 +0100

    Adding NewQuote Component

---
+++ src/components/NewQuote.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="row">
+    <form>
+      <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group">
+        <label>Quote</label>
+        <textarea class="form-control" rows="3"></textarea>
+      </div>
+      <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group">
+        <button class="btn btn-primary" @click="createNewQuote"></button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+    data: function() {
+        return {
+            quote: '',
+        }
+    },
+    methods: {
+        createNewQuote: function() {
+
+        }
+    }
+};
+</script>
+
+<style scoped>
+</style>
